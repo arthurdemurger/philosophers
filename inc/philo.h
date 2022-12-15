2243de860951252d86cac83fcde4934c75cb4da3
lirockk <arthurdemurger26@gmail.com>
2022-12-15 10:42:14 +0100

    change error management

--- inc/philo.h
+++ inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 16:46:39 by ademurge          #+#    #+#             */
-/*   Updated: 2022/12/14 18:16:16 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/12/15 10:40:23 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,15 @@
 # define LEAKS system("leaks philo");
 
 /* Error management */
-# define ARGUMENT_ERROR "Wrong number of arguments."
-# define NUMBER_TOO_BIG "The number entered is too big."
-# define WRONG_TIME_ERROR "Time cannot be negative."
-# define MALLOC_ERROR "There was a problem in a malloc."
-# define MUTEX_ERROR "There was a problem in a mutex."
-# define PTHREAD_ERROR "There was a problem in a thread."
-# define NB_PHILO_ERROR "There must be at least one philo."
-# define MIN_EAT_ERROR "Philosophers cannot eat a negative number of times."
-# define NOT_DIGIT_ERROR "Only enter numbers."
+# define ARGUMENT_ERROR "philo error : wrong number of arguments."
+# define NUMBER_TOO_BIG "philo error : the number entered is too big."
+# define WRONG_TIME_ERROR "philo error : time cannot be negative."
+# define MALLOC_ERROR "philo error : there was a problem in a malloc."
+# define MUTEX_ERROR "philo error : there was a problem in a mutex."
+# define PTHREAD_ERROR "philo error : there was a problem in a thread."
+# define NB_PHILO_ERROR "philo error : incorrect number of philosophers"
+# define MIN_EAT_ERROR "philo error : philosophers cannot eat a negative number of times."
+# define NOT_DIGIT_ERROR "philo error : only enter numbers."
 
 /* Others */
 # define YES 1
@@ -99,7 +99,7 @@ typedef struct s_main
 */
 
 /* Main */
-void		init(int ac, char **av, t_main *main);
+int			init(int ac, char **av, t_main *main);
 void		*routine(void *arg);
 void		end_philo(t_main *main);
 
--- src/exit.c
+++ src/exit.c
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 09:52:25 by ademurge          #+#    #+#             */
-/*   Updated: 2022/12/14 17:37:18 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/12/15 10:39:58 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,23 +30,19 @@ void	end_philo(t_main *main)
 	int	i;
 
 	i = -1;
-	if (main)
-	{
-		while (++i < main->n_phi)
-			if (pthread_mutex_destroy(&main->fork[i]))
-				ft_error(main, MUTEX_ERROR);
-		if (pthread_mutex_destroy(&main->eat))
+	while (++i < main->n_phi)
+		if (pthread_mutex_destroy(&main->fork[i]))
 			ft_error(main, MUTEX_ERROR);
-		if (pthread_mutex_destroy(&main->write))
-			ft_error(main, MUTEX_ERROR);
-		free_all(main);
-	}
+	if (pthread_mutex_destroy(&main->eat))
+		ft_error(main, MUTEX_ERROR);
+	if (pthread_mutex_destroy(&main->write))
+		ft_error(main, MUTEX_ERROR);
+	free_all(main);
 	exit(EXIT_SUCCESS);
 }
 
 void	ft_error(t_main *main, char *type)
 {
-	write (STDERR_FILENO, "Error\n", 6);
 	ft_putendl_fd(type, STDERR_FILENO);
 	free_all(main);
 	exit(EXIT_FAILURE);
